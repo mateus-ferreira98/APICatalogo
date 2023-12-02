9bfebbe052c7c3b52b7a0bfa513826e515dc5581
Mateus <mateusgoias@live.com>
2023-12-02 11:55:05 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = string.Empty;
+
+    public string ImagemUrl { get; set; } = string.Empty;
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace APICatalogo.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+
+    public decimal Preco { get; set; }
+
+    public string ImagemUrl { get; set; } = string.Empty;
+
+    public float Estoque { get; set; }
+
+    public DateTime DataCadastro { get; set; }
+}
